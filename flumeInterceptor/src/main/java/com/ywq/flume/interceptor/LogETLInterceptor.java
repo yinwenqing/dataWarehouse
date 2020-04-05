80ae9a97154c9fcffdabe9b7ff67b05e432c7ea5
wq <1801420446@qq.com>
2020-04-05 22:29:30 +0800

    update

--- flumeInterceptor/src/main/java/com/ywq/flume/interceptor/LogETLInterceptor.java
+++ flumeInterceptor/src/main/java/com/ywq/flume/interceptor/LogETLInterceptor.java
@@ -1,4 +1,74 @@
 package com.ywq.flume.interceptor;
 
-public class LogETLInterceptor {
+
+import org.apache.flume.interceptor.Interceptor;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogETLInterceptor implements Interceptor {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public org.apache.flume.Event intercept(org.apache.flume.Event event) {
+        //1. 获取数据
+        byte[] body=event.getBody();
+
+        String log = new String(body, Charset.forName("UTF-8"));
+
+        // 2. 校验 启动日志（json)    事件日志（服务器事件|json)
+        if(log.contains("start")){
+            //校验启动日志
+            if(LogUtils.valuateStart(log)){
+                return event;
+            }
+        }else{
+            if(LogUtils.valuateEvent(log)){
+                return event;
+            }
+
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public List<org.apache.flume.Event> intercept(List<org.apache.flume.Event> events) {
+        ArrayList<Event> interceptors = new ArrayList<Event>();
+
+        for (org.apache.flume.Event event : events) {
+            Event interceptor1=interceptor(event);
+
+            if(interceptor1!=null){
+                interceptors.add(interceptor1);
+            }
+        }
+        return interceptors;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    public static class Builder implements Interceptor.Builder{
+
+        @Override
+        public Interceptor build() {
+            return new LogETLInterceptor();
+        }
+
+        @Override
+        public void configure(org.apache.flume.Context context) {
+
+        }
+    }
 }
+
+
